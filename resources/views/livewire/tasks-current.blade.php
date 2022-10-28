19f94a6422ea25f3f65059aa3a783bc91703cf6d
Mike Maltby <mike@ingersollrentall.ca>
2022-10-27 21:46:50 -0400

    Add opacity-50 on task wire:loading

--- resources/views/livewire/tasks-current.blade.php
+++ resources/views/livewire/tasks-current.blade.php
@@ -32,7 +32,11 @@
         </script>
     @endpush
 
-    <section class="task-lists bg-slate-900 text-white h-full">
+    <section 
+        class="task-lists bg-slate-900 text-white h-full"
+        wire:loading.class="opacity-50" 
+        wire:target="edit"
+    >
 
         <div class="max-w-7xl mx-auto">
             <div class="flex w-full justify-between">
@@ -138,7 +142,11 @@
 
     </section>
 
-    <x-dialog-modal wire:model="showTaskDialog" maxWidth="7xl" contentClass="lg:overflow-y-hidden">
+    <x-dialog-modal 
+        wire:model="showTaskDialog" 
+        maxWidth="7xl" 
+        contentClass="lg:overflow-y-hidden"
+    >
         
         <x-slot name="title">
 
@@ -147,6 +155,8 @@
                 @resize.window="maybeShow()"
                 x-init="maybeShow()"
                 class="grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-6 gap-3 pt-3 mb-3"
+                wire:loading.class="opacity-50"
+                wire:target="save, resetFormFields"
             >
 
                 <!-- Task Status -->        
@@ -223,7 +233,11 @@
             </div>
 
             <!-- Task Name -->
-            <div class="mb-8 col-span-2 md:col-span-6">
+            <div 
+                class="mb-8 col-span-2 md:col-span-6"
+                wire:loading.class="opacity-50"
+                wire:target="save, resetFormFields"
+            >
                 <x-forms.form-group>
                     <x-forms.input type="text" id="name" name="name" wire:model="currentTask.name" />
                     <x-forms.label for="name">{{ __('Task Name') }}</x-forms.label>
@@ -237,7 +251,11 @@
         
         <x-slot name="content">
 
-            <div class="grid grid-cols-2 gap-x-3 gap-y-8 lg:h-full">
+            <div 
+                class="grid grid-cols-2 gap-x-3 gap-y-8 lg:h-full"
+                wire:loading.class="opacity-50"
+                wire:target="save, resetFormFields"
+            >
 
                 <div class="mb-3 mt-3 col-span-2 lg:col-span-1 lg:overflow-y-auto overflow-x-hidden">
                     <h2 class="mb-3">{{ __('Details') }}</h2>
@@ -296,7 +314,11 @@
                                     />
                                     <x-forms.label for="currentSubtask.{{ $key }}.name" class="hidden">{{ __('Subtask Name') }}</x-forms.label>
                                 </x-forms.form-group>
-                                <button wire:click.prevent="edit({{ $value['id'] }})" class="flex justify-items-center items-center h-6 w-6 fill-gray-300 hover:fill-green-600">
+                                <button 
+                                    wire:click.prevent="edit({{ $value['id'] }})" 
+                                    class="flex justify-items-center items-center h-6 w-6 fill-gray-300 hover:fill-green-600"
+                                    aria-label="Edit Subtask Details"
+                                >
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.8 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg>
                                 </button>
                                 @error ('subtask.{{ $value }}.name')
